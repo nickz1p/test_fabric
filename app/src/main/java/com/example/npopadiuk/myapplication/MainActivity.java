3a21ba0b19c3e184dbe09ca18954fbb29409d96a
Tatyana <tatyana.blagodarova@sigmaukraine.com>
2015-07-15 15:08:59 +0300

    test change
    test

--- app/src/main/java/com/example/npopadiuk/myapplication/MainActivity.java
+++ app/src/main/java/com/example/npopadiuk/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
+
         setContentView(R.layout.activity_main);
     }
 
